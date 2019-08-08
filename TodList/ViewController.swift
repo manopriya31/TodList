93f1baa7fe9daf45a85eb57e5ee595ecc07fa04c
manopriya31 <manopriya31@gmail.com>
2019-08-08 17:15:37 +0530

    Add New Item Functionality complete

--- TodList/ViewController.swift
+++ TodList/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Milk", "Buy Rice"]
+    var itemArray = ["Find Mike", "Buy Milk", "Buy Rice"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    //MARK - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return itemArray.count
     }
@@ -26,6 +27,7 @@ class ViewController: UITableViewController {
       return cell
     }
     
+    //MARK - Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
@@ -38,5 +40,22 @@ class ViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle:.alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textfield.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "Create New"
+            textfield = alertTextfield
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
